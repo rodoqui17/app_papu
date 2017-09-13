2b6702ba65af039f650993d38b84d0b7f86bffa5
rodoqui17 <quiroz.rodrigo@gmail.com>
2017-09-12 23:30:39 -0400

    OneSignal Config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform,AlertCOntroller } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { OneSignal } from '@ionic-native/onesignal';
+
 
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -11,12 +13,31 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
+private oneSignal: OneSignal,
+    private alertCtrl: AlertController
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+       this.handlerNotifications();
     });
   }
+  private handlerNotifications(){
+  this.oneSignal.startInit('f32eebad-3153-4d62-8697-0df6298326e9', '143728972842');
+
+  this.oneSignal.handleNotificationOpened()
+  .subscribe(jsonData => {
+    let alert = this.alertCtrl.create({
+      title: jsonData.notification.payload.title,
+      subTitle: jsonData.notification.payload.body,
+      buttons: ['OK']
+    });
+    alert.present();
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  });
+  this.oneSignal.endInit();
+}
 }
