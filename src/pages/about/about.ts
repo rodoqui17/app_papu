3f2b2ceab91bb8315b4fc65e174d89bd1623b4bc
rodoqui17 <quiroz.rodrigo@gmail.com>
2017-09-14 04:41:29 -0400

    Paneles

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+MyParam:string;
+Habilidad:string;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+ this.MyParam =this.navParams.get('status');
+ this.Habilidad =this.navParams.get('habilidad');
+ alert(this.MyParam);
+ alert(this.Habilidad);
 
-  constructor(public navCtrl: NavController) {
-
-  }
+ }
 
 }
--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Platform, AlertController } from 'ionic-angular';
+import { OneSignal } from '@ionic-native/onesignal';
+import { AboutPage } from '../about/about';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
+
+
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,private oneSignal: OneSignal,
+private alertCtrl: AlertController) {
 					
   }
-nombre_propiedad:string = 'Esto es un test';
+
+   
+perfil(){
+	this.navCtrl.push(AboutPage, { status: 'Rodrigo',habilidad: 'cocinar'})
+
 }
+
+
+nombre_propiedad:string = 'Esto es un test';
+
+}
+
+
