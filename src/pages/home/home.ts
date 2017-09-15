4f1f96f11a44a857b7e1e5925d5986dfd45204de
rodoqui17 <quiroz.rodrigo@gmail.com>
2017-09-15 03:47:31 -0400

    home edit button

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ContactPage } from '../contact/contact';
 
 @Component({
   selector: 'page-home',
@@ -7,15 +8,22 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-	
+
    constructor(public navCtrl: NavController) {
 
   }
+
+
 
 
 fechaCorta: string = new Date().toISOString();
   fecha: string = this.fechaCorta;
   minFecha: string = (new Date().getFullYear()-5).toString();
   maxFecha: string = (new Date().getFullYear()+5).toString();
+
+  buscar(){
+	this.navCtrl.push(ContactPage, { status: 'Rodrigo',habilidad: 'cocinar'})
+
+}
 
 }
