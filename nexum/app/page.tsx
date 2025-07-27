f19578f71fa9590a2b43a2c74b0d4ba6c1d2aaa0
Rodrigo Quiroz <quiroz.rodrigo@gmail.com>
2025-07-27 11:20:39 -0400

    update

--- nexum/app/page.tsx
+++ nexum/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly now here.
+            Save and see your changes instantly.
           </li>
         </ol>
 
